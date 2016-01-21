42e42c0d4026c3b1d0c25c7b081c1be45bf52bb7
Al <alastair.harry@gmail.com>
2016-01-21 13:10:28 +0800

    Amended MMI Growth model script to make it more efficient.
    Confidence intervals are calculated using using Taylor expansion rather than bootstrap
    
    Began impelenting Version 2 of this script that uses nlminb rather than nls to improve convergance.

--- MMI Growth model 2.R
+++ MMI Growth model 2.R
@@ -1,19 +1,22 @@
 ###############################################################################
-# 19/01/2016
+# 11/01/2016
 # Author: Alastair V Harry - alastair.harry@gmail.com
 # Deptartment of Fisheries Western Australia & 
 # Centre For Sustainable Tropical Fisheries & Aquaculture
 # James Cook University, Townsville
-# 
+#
+# From Harry et al (2013) Age, growth, and reproductive biology of the spot-tail
+# shark, Carcharhinus sorrah, and the Australian blacktip shark, C. tilstoni, 
+# from the Great Barrier Reef World Heritage Area, north-eastern Australia. 
+# Marine and Freshwater Research
+#
 # Description: An R-script for doing a multi-model analysis of fish growth
 # and plotting with confidence intervals. This version includes six
-# deterministic growth models. Style of outputs follows Walker (2005) 
-# `Reproduction in fisheries science.`
-#
-# Example data are used from: Harry et al (2013) Age, growth, and reproductive 
-# biology of the spot-tail shark, Carcharhinus sorrah, and the Australian
-# blacktip shark, C. tilstoni, from the Great Barrier Reef World Heritage Area,
-# north-eastern Australia. Marine and Freshwater Research
+# deterministic growth models. Obviously more can be included 
+# although the section that does bootstrapping and confidence intervals 
+# is only set up to plot models that have 2 or 3 parameters at the moment, so a 
+# model with more parameters probably isn't going to work. Style of outputs
+# follows Walker (2005) Reproduction in fisheries science. 
 #
 ################################################################################
 
@@ -26,14 +29,15 @@ library(RCurl)
 library(ggplot2)
 library(dplyr)
 library(propagate)
+source("Models.R")
 
 # Load example data from github
 # If sss.verifypeer = TRUE doesn't work, set to false, but read this:
 # http://ademar.name/blog/2006/04/curl-ssl-certificate-problem-v.html
-#data <- getURL("https://raw.githubusercontent.com/alharry/spot-tail/master/SSS.csv",ssl.verifypeer = FALSE)%>%
-#        textConnection()%>%
-#        read.csv(sep=",")%>%
-#        tbl_df()
+data <- getURL("https://raw.githubusercontent.com/alharry/spot-tail/master/SSS.csv",ssl.verifypeer = FALSE)%>%
+  textConnection()%>%
+  read.csv(sep=",")%>%
+  tbl_df()
 data<-read.csv("SSS.csv")
 # Skip to here and load your data if you don't want to use the 
 # example dataset
@@ -48,8 +52,8 @@ sex="f"
 Mini<-L0<-525
 
 # Specify dimensions for plotting (used at the end of this script)
-Max.Overall<-data$STL%>%na.omit%>%max()%>%ceiling() 
-Max.Plot<-data$AgeAgree%>%na.omit%>%max()%>%ceiling()
+Max.Overall<-ceiling(max(na.omit(data$STL)))
+Max.Plot<-ceiling(max(na.omit(data$AgeAgree)))
 
 # Specify subset of data to be used (i.e. males or females or both) and keep
 # an extra data frame with all the data (data1)
@@ -58,7 +62,7 @@ data<-if(sex=="both"){data}else{filter(data,Sex==sex)}
 Max<-max(na.omit(data$STL)) 
 Max.Age<-max(na.omit(data$AgeAgree))
 
-# Routine for getting starting values for asymptotic growth models using Ford-Walford method
+# Define starting values for asymptotic growth models using Ford-Walford method
 # for Linf and K. To estimate L0, a quadratic is fit to the mean length at
 # age data, and the intercept extracted. This section I got from Derek Ogle's
 # FishR. 
@@ -73,10 +77,12 @@ start.L0<-lm(mean.age~poly(as.numeric(names(mean.age)), 2, raw=TRUE))$coef[1]
 # Remove all non-essential data from the data frame
 data<-na.omit(dplyr::select(data,STL,AgeAgree))
 names(data)<-c("STL","Age")
+xy<-data
+names(xy)<-c("y","x")
 
 # Create a set of candidate models for MMI comparison. 
 # Three commonly used non-linear growth models are listed below.
-# The functions have each been solved in such a way that
+# The functions have each been solved (by someone else) in such a way that
 # they explicitly include the y-intercept (L0, length at time zero) as a
 # fitted model parameter, and differ from the more common form that explicitly
 # includes the x-intercept (t0, hypothetical age at zero length). The L0 
@@ -88,15 +94,23 @@ names(data)<-c("STL","Age")
 # Constraining the model to have only 2 parameters instead of 3 causes a bias
 # in the other parameters, so if you have good data for all sizes its best to
 # use the 3 parameter version. 
-# In my opinion it doesn't make much sense to include both 2 and 3 parameter
-# versions of the same model in your set of candidate models.
+# IMO it doesn't make much sense to include both 2 and 3 parameter versions of the
+# same model in your set of candidate models.
+
+# G1 - 3 parameter VB
+
+
+mo<- VB3
+P<-c(b1=start.linf[[1]],b2=start.L0[[1]],b3=start.k[[1]])
+Test<-fit(mo,P,xy)
+
 
 #G1 - 3 parameter VB
 g1<-STL~abc2+(abc1-abc2)*(1-exp(-abc3*Age))
 g1.Start<-list(abc1=start.linf,abc2=start.L0,abc3=start.k)
 g1.name<-"VB3"
 # G2 - 2 parameter VB
-g2<- substitute(STL~L0+(ab1-L0)*(1-exp(-ab2*Age)),list(L0=L0))
+g2<- STL~ L0+(ab1-L0)*(1-exp(-ab2*Age))
 g2.Start<-list(ab1=start.linf,ab2=start.k)
 g2.name<-"VB2"
 # G3 - 3 parameter Gompertz
@@ -104,7 +118,7 @@ g3<- STL~ abc2*exp(log(abc1/abc2)*(1-exp(-abc3*Age)))
 g3.Start<-list(abc1=start.linf,abc2=start.L0,abc3=start.k)
 g3.name<-"GOM3"
 # G4 - 2 parameter Gompertz
-g4<- substitute(STL~L0*exp(log(ab1/L0)*(1-exp(-ab2*Age))),list(L0=L0))
+g4<- STL~L0*exp(log(ab1/L0)*(1-exp(-ab2*Age)))
 g4.Start<-list(ab1=start.linf,ab2=start.k)
 g4.name<-"GOM2"
 # G5 - 3 parameter Logistic model
@@ -112,7 +126,7 @@ g5<- STL~ (abc1*abc2*exp(abc3*Age))/(abc1+abc2*((exp(abc3*Age))-1))
 g5.Start<-list(abc1=start.linf, abc2=start.L0, abc3=start.k)
 g5.name<-"LOGI3"
 # G6 - 2 parameter Logistic model
-g6<- substitute(STL~ (ab1*L0*exp(ab2*Age))/(ab1+L0*((exp(ab2*Age))-1)),list(L0=L0))
+g6<- STL~ (ab1*L0*exp(ab2*Age))/(ab1+L0*((exp(ab2*Age))-1))
 g6.Start<-list(ab1=start.linf,ab2=start.k)
 g6.name<-"LOGI2"
 
@@ -134,65 +148,112 @@ Results[[5]]<-nls(Models[[5]],data=data,start=Start[[5]],algorithm="port")
 Results[[6]]<-nls(Models[[6]],data=data,start=Start[[6]],algorithm="port")
 
 # Put best fit paramters into a matrix
-par.matrix<-data.frame(matrix(NA,Mods,6))
-names(par.matrix)<-c('abc1','SE','abc2','SE','abc3','SE')
+par.matrix<-data.frame(matrix(NA,Mods,3))
+names(par.matrix)<-c('abc1','abc2','abc3')
 for(i in 1:Mods){
-	npars=length(coef(Results[[i]]))*2
-	mod.pars<-c(t(summary(Results[[i]])$par[,c(1,2)]))
-	      for(j in 1:npars){
-		    par.matrix[i,j]<-mod.pars[j]
-		  }
-		}
+  npars=length(coef(Results[[i]]))
+  for(j in 1:npars)
+    par.matrix[i,j]<-coef(Results[[i]])[j]
+}
 
 # Look at diagnostic plots
-# for(i in 1:Mods){plot(nlsResiduals(Results[[i]]))}
+for(i in 1:Mods){plot(nlsResiduals(Results[[i]]))}
+
 
 # Run the multi-model inference comparison. The following section calculates
 # AIC values, AIC differences, AIC weights and residual standard errors
-AIC.vals<-lapply(Results,AIC)%>%unlist()
-Residual.Standard.Error<-lapply(Results,function(x){(sum(residuals(x)^2)/df.residual(x))%>%sqrt()})%>%unlist()
+AIC.vals<-rep(NA,Mods)
+Residual.Standard.Error<-rep(NA,Mods)
+for(s in 1:Mods){AIC.vals[s]<-AIC(Results[[s]])}
 AIC.dif<-AIC.vals-min(AIC.vals)
 AIC.weight= round((exp(-0.5*AIC.dif))/(sum(exp(-0.5*AIC.dif))),4)*100
+for(t in 1:Mods){Residual.Standard.Error[t]<-summary(Results[[t]])$sigma}
 
-# Store output of MMI analysis. This is the important bit.
-MMI.Analysis<-data.frame(`AIC`=AIC.vals,`Delta AIC`=AIC.dif,w=AIC.weight,RSE=Residual.Standard.Error,
-		par.matrix, row.names=Mod.names)%>%
-    signif(4) # Round to four significant figures
+# Store output of MMI analysis. This is the important bit. Beyond this section
+# its all plotting and confidence intervals, which may have a tendency not 
+# to work so well depending on the sparsity of your data. It's somewhat
+# 'optional' beyond here anyway, since many people don't present
+# confidence intervals with their growth models anyway. 
+MMI.Analysis<-data.frame(AIC.vals,AIC.dif,AIC.weight,Residual.Standard.Error,
+                         par.matrix, row.names=Mod.names)
+names(MMI.Analysis)[1:4]<-c("AIC", "AIC dif", "w","RSE")
 
-# Calculate confidence and prediction intervals and plot output
+# Calculate bootstrap confidence intervals. First step is to bootstrap the data. 
+# The nlsBoot function takes the data and randomly re-samples it a certain
+# number of times. 'Iter' is the number of interations bootstraps to be run. 
+# I usually do 10000 in the final analysis but this will take a while.
+# The actual confidence intervals can be extracted from the 'Bootstrapping'
+# list e.g. to extract CI for LOGI3 type Bootstrapping[[3]]$bootCI
+Iter=1000
+Bootstrapping<-list()
+for(i in 1:length(Models)){Bootstrapping[[i]]<-nlsBoot(Results[[i]],Iter)}
+
+# Create a matrix with 95% confidence intervals
+Conf.Ints<-data.frame(matrix(NA,length(Models),6),row.names=Mod.names)
+names(Conf.Ints)[1:length(Conf.Ints[1,])]<-c("2.5%","97.5%")
+for(i in 1:length(Models)){
+  npars=nrow(Bootstrapping[[i]]$bootCI)
+  for(j in 1:(npars*2)){Conf.Ints[i,j]<-c(t(Bootstrapping[[i]]$bootCI[,-1]))[j]}}
+
+# Store all necessary outputs from analysis together
+MMI.Analysis<-cbind(MMI.Analysis,Conf.Ints)
+
+# Round to four significant figures
+MMI.Analysis<-signif(MMI.Analysis,4)
 
 # Specify values of age that you want to predict length over
-newAge=seq(0,Max.Age,length.out=50)
+x.deter<-seq(0,Max.Age,0.1)
 
-# Confidence intervals (1-ALPHA)
-ALPHA<-0.05
+# Number of iterations is now the number of successful bootstraps (i.e.
+# original Iter minus those bootstraps that didn't converge)
 
-# Model with lowest AIC value for plotting
-bestMod<-Results[[which(AIC.dif==0)]]
+Iter<-nrow(Bootstrapping[[which(AIC.dif==0)]]$coefboot)
 
-# Calculate confidence intervals using 2nd order Taylor expansion from
-# propagate package
-Output<-data.frame(Age=newAge,
-                   predictNLS(bestMod,newdata=data.frame(Age=newAge),alpha=ALPHA,
-                   interval="confidence",do.sim=FALSE)$summary[,c(1,3,5,6)])
-names(Output)=c("Age","STL","SD","cLower","cUpper")
+# This bit calculates the actual confidence intervals and prediction intervals
+# for the 'best fit' model
+for(k in which(AIC.dif==0)){
+  plotmatrix<-matrix(NA,ncol=length(x.deter),nrow=Iter)
+  
+  Npars<-length(Start[[which(AIC.dif==0)]])
+  
+  for(l in (1:Iter)){
+    abc1<-as.numeric(Bootstrapping[[k]]$coef[l,1])
+    abc2<-if(Npars==3){as.numeric(Bootstrapping[[k]]$coef[l,2])}else{L0}
+    abc3<-if(Npars==3){as.numeric(Bootstrapping[[k]]$coef[l,3])}
+    else{as.numeric(Bootstrapping[[k]]$coef[l,2])}
+    # Quick add on to do confidence intervals in 2 parameter models
+    ab1<-abc1
+    ab2<-abc3
+    Age=x.deter
+    
+    plotmatrix[l,]<-formula(formula(Results[[k]])[[3]])
+  }
+  
+  RSE<-Bootstrapping[[k]]$rse
+  boundsmatrix<-matrix(NA,nrow=4,ncol=length(x.deter))
+  
+  for(m in 1:length(x.deter)){
+    boundsmatrix[1,m]<-quantile(plotmatrix[,m],0.025)
+    boundsmatrix[2,m]<-quantile(plotmatrix[,m],0.975)
+    boundsmatrix[3,m]<-quantile((plotmatrix-RSE)[,m],0.025)
+    boundsmatrix[4,m]<-quantile((plotmatrix+RSE)[,m],0.975)
+  }
+}
 
-# Calculate predition intervals
-Output$pLower<-Output$STL-qt(1 - ALPHA/2, df.residual(bestMod))  * sqrt(Output$SD^2 + summary(bestMod)$sigma^2)
-Output$pUpper<-Output$STL+qt(1 - ALPHA/2, df.residual(bestMod))  * sqrt(Output$SD^2 + summary(bestMod)$sigma^2)
-
+# Save outputs
+y.deter<-predict(Results[[which(AIC.dif==0)]],list(Age=x.deter),
+                 type="response")
+Output<-data.frame(Age=x.deter,TL=y.deter,clower=boundsmatrix[1,],cupper=boundsmatrix[2,],
+                   plower=boundsmatrix[3,],pupper=boundsmatrix[4,])
+Output<-Test$confInts
 # Plot data
-p<-ggplot(Output,aes(x=Age,y=STL))+geom_line()+
-  # confidence intervals
+p<-ggplot(Output,aes(x=x,y=Mean))+geom_line()+
   geom_line(aes(y=cUpper),linetype="dashed")+geom_line(aes(y=cLower),linetype="dashed")+
-  # prediction intervals
   geom_line(aes(y=pUpper),linetype="dotted")+geom_line(aes(y=pLower),linetype="dotted")+
-  # raw data
-  geom_point(data=data,aes(x=Age,y=STL))+xlab("Age (Years)")+ylab("TL (cm)")+
+  geom_point(data=Test$data,aes(x=x,y=y))+xlab("Age (Years)")+ylab("TL (cm)")+
   theme(panel.background=element_blank())+theme_classic()+xlim(0,Max.Plot)+ylim(Mini*0.9,Max.Overall*1.1)
-p
 
 # Save analysis
 #write.csv(MMI.Analysis, "MMI.Analysis.csv")
-Results<-list(MMI.Analysis,bestMod,Output)
+Results<-list(MMI.Analysis,Output)
 
--- Models.R
+++ Models.R
@@ -1,7 +1,8 @@
 # Generic model object that returns the gradient vector and Hessian matrix
-# Stolen frmo here:
+# Adapted from here:
 # http://oddhypothesis.blogspot.com.au/2014/08/optimizing-with-r-expressions.html
 
+# Generic model object 
 ModelObject = setRefClass('ModelObject', 
                           fields = list(
                             name = 'character',
@@ -25,11 +26,110 @@ ModelObject = setRefClass('ModelObject',
                             hessian = function(p, data){
                               J = jacobian(p, data)
                               return(J %*% t(J))
+                            },
+                            dev =function(p, data){
+                              eval(.self$expr, c(as.list(p), as.list(data)))-data$y
                             }
                           )
 )
 
-mo = ModelObject(
-  name = 'gompertz', 
-  expr = expression( y0*exp(A*exp(-exp((u*exp(1)/A)*(l-x)+1))) )
+# Function for getting best fit parameters and other relevant info
+fit = function(mo,p,data){
+  # Solve for best fit paramters
+  Opt<-nlminb(p, function(p, data){
+    r = data$y - mo$value(p,data)
+    sum(dnorm(data$y,mean=mo$value(p,data),sd=SD,log=T))
+    return(r %*% r)
+  }, gradient = mo$gradient, hessian = mo$hessian, data=data)
+  # Residuals
+  Opt$residuals<-mo$dev(Opt$par,data)
+  # Residual degrees of freedom
+  Opt$df.residual<-dim(data)[1]-length(p)
+  # Sigma
+  Opt$sigma<-sqrt(sum(Opt$residuals^2)/Opt$df.residual)
+  # Hessian matrix
+  Opt$hessian<-mo$hessian(Opt$par,data)
+  # Gradient
+  Opt$gradient<-mo$gradient(Opt$par,data)
+  # Variance covariance matrix
+  Opt$vcov<-solve(0.5*(Opt$hessian + t(Opt$hessian)))*Opt$sigma^2
+  # Paramter standard errors
+  Opt$se<-sqrt(diag(Opt$vcov))
+  # Organise parameters and standard errors
+  Opt$parlist<-list()
+    for(i in 1:length(Opt$par)){
+      Opt$parlist[[i]]<-c(Opt$par[i],Opt$se[i])
+      names(Opt$parlist)[i]<-paste(names(Opt$par[i]))
+    }
+  Opt$parlist<-data.frame(Opt$parlist)
+  # 
+  Opt$confInts<-predictNLMINB(Opt,data)
+  Opt$data<-data
+  return(Opt)
+}
+
+
+VB3 = ModelObject(
+  name = 'VB3', 
+  expr = expression( b2+(b1-b2)*(1-exp(-b3*x)) )
 )
+
+# Function for propagating uncertainty and getting 95% confidence intervals
+#confInts<-Vectorize(function(x){
+#  List<-Opt$parlist
+#  List$x=c(x,0)
+#  conf<-t(propagate(expr=mo$expr,data=List,type="stat",do.sim=FALSE)$prop[c(1,3,5,6)])
+#  predLower<-conf[1]+sqrt(conf[2]^2+Opt$sigma^2)*qt(0.025,Opt$df.residual)
+#  predUpper<-conf[1]+sqrt(conf[2]^2+Opt$sigma^2)*qt(0.975,Opt$df.residual)
+#  conf<-c(conf,predLower,predUpper)
+#  return(conf)
+#})
+
+# A hacked version of the predictNLS function to work with NLMINB output
+predictNLMINB<-function(Opt,data){
+  newdata<-data.frame(x=seq(min(data$x),max(data$x),length.out=50))
+  ALPHA=0.05
+  EXPR<-mo$expr
+  VARS <- all.vars(EXPR)
+  COEF <- Opt$par
+  predVAR <- setdiff(VARS, names(COEF))
+  if (!identical(names(newdata)[1:length(predVAR)], predVAR)) 
+    stop("newdata should have name(s) ", predVAR, "!\n")
+  VCOV <- Opt$vcov
+  NR <- NROW(newdata)
+  outMAT <- matrix(nrow = NR, ncol = 6)
+  propLIST <- vector("list", length = NR)
+  for (i in 1:NR) {
+    tempDATA <- newdata[i, ]
+    names(tempDATA) <- colnames(newdata)
+    SEL <- which(names(tempDATA) == predVAR)
+    predVEC <- c(COEF, as.numeric(tempDATA[SEL]))
+    names(predVEC) <- c(names(COEF), predVAR)
+    DF <- rbind(predVEC, 0)
+    row.names(DF) <- NULL
+    if (NCOL(tempDATA) == length(predVAR)){
+      forCOV <- rep(0, length(predVAR))}else
+        {forCOV <- tempDATA[, (length(predVAR) + 1):(2*length(predVAR))]}
+    COV <- VCOV
+    for (k in 1:length(forCOV)) {
+      COV <- mixCov(COV, forCOV[k])
+    }
+    SEL <- tail(1:nrow(COV), length(predVAR))
+    dimnames(COV)[[1]][SEL] <- dimnames(COV)[[2]][SEL] <- predVAR
+    PROP <- propagate(expr = EXPR, data = DF, use.cov = COV, 
+                      alpha = ALPHA, do.sim = FALSE)
+    propLIST[[i]] <- PROP
+    outPROP <- PROP$prop
+    MEAN<-outPROP[2]
+    SD<-outPROP[4]
+    TQUAN <- qt(1 - ALPHA/2, Opt$df.residual)
+        outPROP[5] <- MEAN - TQUAN * SD
+        outPROP[6] <- MEAN + TQUAN * SD
+        outPROP[7] <- MEAN - TQUAN * sqrt(SD^2 + Opt$sigma^2)
+        outPROP[8] <- MEAN + TQUAN * sqrt(SD^2 + Opt$sigma^2)
+    outMAT[i, ] <- outPROP[-c(1,3)]
+  }
+  outMAT <- as.data.frame(cbind(newdata,outMAT))
+  colnames(outMAT) <- c("x","Mean","SD","cLower","cUpper","pLower","pUpper") 
+  return(outMAT)
+}
